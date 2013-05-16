6ca215da1673e92ba726b5c7085be79420091334
Stephan Renatus <s.renatus@cloudbau.de>
2013-05-16 10:47:49 +0200

    Make search query a node attribute
    defaulting to the old behavior

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,16 @@
 default[:xtreemfs][:user] = "xtreemfs"
 default[:xtreemfs][:group] = "xtreemfs"
 
+# DIR defaults
+
+default[:xtreemfs][:dir][:bind_ip] = node[:ipaddress]
+default[:xtreemfs][:dir][:query] = 'xtreemfs_dir_service:true'
+
 # MRC defaults
 
 default[:xtreemfs][:mrc][:dir_service_host] = "localhost"
+default[:xtreemfs][:mrc][:query] = 'xtreemfs_mrc_service:true'
+default[:xtreemfs][:mrc][:bind_ip] = node[:ipaddress]
 
 # OSD defaults
 
@@ -12,8 +19,6 @@ default[:xtreemfs][:osd][:count] = 1
 default[:xtreemfs][:osd][:first_listen_port] = 32640
 default[:xtreemfs][:osd][:first_http_port] = 30640
 default[:xtreemfs][:osd][:bind_ip] = node[:ipaddress]
-default[:xtreemfs][:mrc][:bind_ip] = node[:ipaddress]
-default[:xtreemfs][:dir][:bind_ip] = node[:ipaddress]
 
 # We don't want to get to fancy with configuration so we use the following convention:
 # "object_base_directory" is configured here and the OSD dirs will be based on the OSD number
@@ -28,3 +33,4 @@ default[:xtreemfs][:dir][:bind_ip] = node[:ipaddress]
 # So just mount your disks to these directories and xtreemfs will handle each disk(set)
 # mounted in these directories with one OSD
 default[:xtreemfs][:osd][:object_base_directory] = "/var/lib/xtreemfs/objs/"
+
