2784d9ac536e6ccc33fca92486bb6f219498f885
Stephan Renatus <s.renatus@cloudbau.de>
2013-05-22 17:57:50 +0200

    add license to README and metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "xtreemfs"
 maintainer       "Hendrik Volkmer"
 maintainer_email "h.volkmer@cloudbau.de"
-license          "All rights reserved"
+license          "Apache v2.0"
 description      "Installs/Configures xtreemfs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
