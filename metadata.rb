87bcb55a7e1c7325af662881d78198722fedd8a2
Stephan Renatus <s.renatus@cloudbau.de>
2013-07-02 18:37:25 +0200

    bump minor version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "h.volkmer@cloudbau.de"
 license          "Apache v2.0"
 description      "Installs/Configures xtreemfs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 
 depends "apt"
