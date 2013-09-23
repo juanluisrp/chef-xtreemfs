5e9dc1846cac977cb019676e041b95974abd5ae4
Stephan Renatus <s.renatus@cloudbau.de>
2013-09-23 16:39:33 +0200

    bugfix: take yourself if you're a service host

--- libraries/get_service_hosts.rb
+++ libraries/get_service_hosts.rb
@@ -1,5 +1,5 @@
 def get_service_hosts(srv)
-  if node[:xtreemfs][:standalone]
+  if node[:xtreemfs][:standalone] or node[:xtreemfs][srv][:service]
     { node[:xtreemfs][srv][:bind_ip] => node[:xtreemfs][srv][:listen_port] }
   else
     Hash[search(:node, "xtreemfs_#{srv}_service:true").map {|n|  [ n[:xtreemfs][srv][:bind_ip], n[:xtreemfs][srv][:listen_port] ] } ]
